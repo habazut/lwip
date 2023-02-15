004ca431b697cbd481223e92bd074fc97cdb7704
xueyunfei <xueyunfei@espressif.com>
2023-02-15 11:25:33 +0800

    tcp_in/ooseq: Fix incorrect segment trim when FIN moved
    In case FIN flag was present in the *next* segment,
    it's copied to the current *inseg* and thus it's length
    needs to be adjusted, so the segment is trimmed correctly.

--- src/core/tcp_in.c
+++ src/core/tcp_in.c
@@ -1528,9 +1528,8 @@ tcp_receive(struct tcp_pcb *pcb)
             if (next &&
                 TCP_SEQ_GT(seqno + tcplen,
                            next->tcphdr->seqno)) {
-              /* inseg cannot have FIN here (already processed above) */
               inseg.len = (u16_t)(next->tcphdr->seqno - seqno);
-              if (TCPH_FLAGS(inseg.tcphdr) & TCP_SYN) {
+              if (TCPH_FLAGS(inseg.tcphdr) & TCP_SYN || TCPH_FLAGS(inseg.tcphdr) & TCP_FIN) {
                 inseg.len -= 1;
               }
               pbuf_realloc(inseg.p, inseg.len);
